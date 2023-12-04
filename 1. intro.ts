a3c7d0a3e8f54fa5399bdfe0236852e513c76f81
geekAshish <ashishkushwaha472004@gmail.com>
2023-12-04 12:07:42 +0530

    typescript

--- 1. intro.ts
+++ 1. intro.ts
@@ -1,12 +1,23 @@
+
+
 let firstName: string = "Ashish kushwaha";
-let age: number = 9;
+firstName.slice(0, 5) // it will only suggest you string related methods
 
 console.log(firstName);
 
-firstName.slice(0, 5) // it will only suggest you string related methods
 
+
+let age: number = 9;
 age.toFixed() // it will only suggest you number related methods
 
 
+let isLoggedIn: boolean = true;
+console.log(isLoggedIn);
+
+
+// type inference
+// If you'r assigning value at the same time, you don't need to define type
+let dob = 55;
+dob = 'fifty-five';
 
 export {}
