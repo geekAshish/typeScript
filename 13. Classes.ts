2f021596feadb2994b95de1c9dadad0bdd6332f3
geekAshish <ashishkushwaha472004@gmail.com>
2023-12-08 23:36:44 +0530

    Private Public classes

--- 13. Classes.ts
+++ 13. Classes.ts
@@ -1,12 +1,31 @@
+// class User {
+//   name: string;
+//   public email: string; // If you don't define it like that, it'll be public always
+//   private id: number = 9; // default value
+//   readonly googleId: number;
+//   constructor(name: string, email: string, id: number, googleId: number) {
+//     this.email = email;
+//     this.name = name;
+//     this.id = id;
+//   }
+// }
+
+// can write the same code like that
 class User {
-  email: string;
-  name: string;
-  id: number;
-  constructor(name: string, email: string, id: number) {
+
+  constructor(
+    public name: string,
+    public email: string,
+    private id: number,
+    readonly googleId: number
+  ) {
     this.email = email;
     this.name = name;
     this.id = id;
   }
+  
 }
 
-const person = new User('ashish', 'a@a.com', 334);
+
+
+const person = new User('ashish', 'a@a.com', 334, 987);
