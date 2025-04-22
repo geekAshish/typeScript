25874e15fa51b219d971e09d2fc0f58bd483fc6d
Ashish Kushwaha <ashish@wishfy.club>
2025-04-22 20:48:36 +0530

    exclude

--- 19. Pick.ts
+++ 19. Pick.ts
@@ -7,8 +7,13 @@ interface User {
   isAdmin: boolean;
 }
 
+type EventType = "click" | "scroll" | "mousemove";
+
 // Pick
 type UpdatedProps = Pick<User, "id" | "name" | "age">;
+
+// Omit
+type ExcludeEvent = Exclude<EventType, "scroll">; // 'click' | 'mousemove'
 
 function updateUserProfile(userData: UpdatedProps) {
   // hit database and update the user
