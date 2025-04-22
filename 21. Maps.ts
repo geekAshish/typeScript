25793984a9e299b632581bc652990964b54f05e8
Ashish Kushwaha <ashish@wishfy.club>
2025-04-22 20:43:30 +0530

    maps

--- 21. Maps.ts
+++ 21. Maps.ts
@@ -12,3 +12,4 @@ usersMap.set("xyz789", { id: "xyz789", name: "Jane Doe" });
 
 // Accessing a value using .get
 console.log(usersMap.get("abc123")); // Output: { id: 'abc123', name: 'John Doe' }
+usersMap.delete("abc123");
